cff07f3078a30c1513e84d6227e5e9f828475593
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2020-05-31 16:03:50 +0100

    Rotas, novos componentes e início da página Serviços

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component' ;
+import { ServicosComponent } from './Components/servicos/servicos.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { ContatoComponent } from './Components/contato/contato.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'servicos',
+    component: ServicosComponent
+  },
+  {
+    path:'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path:'contato',
+    component: ContatoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { FooterComponent } from './Components/footer/footer.component';
 import { HomeComponent } from './Components/home/home.component';
+import { ServicosComponent } from './Components/servicos/servicos.component';
+import { PortfolioComponent } from './Components/portfolio/portfolio.component';
+import { ContatoComponent } from './Components/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ServicosComponent,
+    PortfolioComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
